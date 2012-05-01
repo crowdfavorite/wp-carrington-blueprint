b7629446a93859f3334027858faa48db7bf25fef
jaredkc <jared@crowdfavorite.com>
2012-04-30 21:58:56 -0600

    adding prev/next links via nav-post in misc directory. Good example what to use misc/ for

---
+++ misc/nav-posts.php
@@ -0,0 +1,23 @@
+<?php
+
+// This file is part of the Carrington Blueprint Theme for WordPress
+//
+// Copyright (c) 2008-2012 Crowd Favorite, Ltd. All rights reserved.
+// http://crowdfavorite.com
+//
+// Released under the GPL license
+// http://www.opensource.org/licenses/gpl-license.php
+//
+// **********************************************************************
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+// **********************************************************************
+
+if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
+if (CFCT_DEBUG) { cfct_banner(__FILE__); }
+?>
+<div class="pagination">
+	<span class="next"><?php next_posts_link(__('Next &raquo;', 'carrington-blueprint')) ?></span>
+	<span class="previous"><?php previous_posts_link(__('&laquo; Previous', 'carrington-blueprint')) ?></span>
+</div>
