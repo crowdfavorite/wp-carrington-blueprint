b45db2504997785f81506691a2f728e73c274294
Jared Cornwall <jared@crowdfavorite.com>
2012-07-11 11:08:53 -0600

    register default sidebar

--- functions/sidebars.php
+++ functions/sidebars.php
@@ -11,7 +11,7 @@
 // **********************************************************************
 // This program is distributed in the hope that it will be useful, but
 // WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 // **********************************************************************
 
 if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
@@ -21,14 +21,22 @@ if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
  * @uses register_sidebar
  */
 function cfct_widgets_init() {
-	// Copy the following code and replace values to create more widget areas
+
 	$sidebar_defaults = array(
 		'before_widget' => '<aside id="%1$s" class="widget clearfix %2$s">',
 		'after_widget' => '</aside>',
 		'before_title' => '<h1 class="widget-title">',
 		'after_title' => '</h1>'
 	);
-	
+
+	// Copy the following code and replace values to create more widget areas
+	register_sidebar(array_merge($sidebar_defaults, array(
+		'id' => 'sidebar-default',
+		'name' => __('Default Sidebar', 'carrington-blueprint'),
+	)));
+
 }
+
 add_action( 'widgets_init', 'cfct_widgets_init' );
+
 ?>
