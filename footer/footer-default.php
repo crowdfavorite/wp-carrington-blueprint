8598703ee33a6b456aab5521d7cae09046f053f6
Jared Cornwall <jared@crowdfavorite.com>
2012-07-27 13:13:10 -0600

    output copyright set in admin in footer

--- footer/footer-default.php
+++ footer/footer-default.php
@@ -22,13 +22,17 @@ if (CFCT_DEBUG) { cfct_banner(__FILE__); }
 	<hr>
 	<footer id="footer" class="site-footer" role="contentinfo">
 <?php
-if (cfct_get_option('cfct_credit') == 'yes') {
+
+	if (cfct_get_option('cfct_copyright') !== null) {
+		echo '<p>'.cfct_get_option('cfct_copyright').'</p>';
+	}
+
+	if (cfct_get_option('cfct_credit') == 'yes') {
 ?>
 		<p id="developer-link"><?php printf(__('Development by <a href="http://crowdfavorite.com" title="Custom WordPress development, design and consulting services." rel="developer designer">%s</a>', 'carrington-blueprint'), 'Crowd Favorite'); ?></p>
 <?php
-}
+	}
 ?>
-		<p>&copy; <?php echo date("Y").' '; bloginfo('name') ?>. All Rights Reserved.</p>
 	</footer><!--#footer -->
 
 </div><!-- .container -->
