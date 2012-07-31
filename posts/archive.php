d96a6ba23a2a909a3bdb8856fc771947b280ed8d
Jared Cornwall <jared@crowdfavorite.com>
2012-07-31 09:37:10 -0600

    better archive template and add pagination

--- posts/archive.php
+++ posts/archive.php
@@ -11,7 +11,7 @@
 // **********************************************************************
 // This program is distributed in the hope that it will be useful, but
 // WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 // **********************************************************************
 
 if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
@@ -21,12 +21,43 @@ get_header();
 ?>
 
 <div id="primary" class="c6-1234">
-	<?php
-	
-	// For the loop used, look in /loops
-	cfct_loop();
+	<h1 class="archive-title"><?php
+		if (is_day()) {
+			printf(__('Daily Archives: %s', 'carrington-blueprint'), '<span>' . get_the_date() . '</span>');
+		} elseif (is_month()) {
+			printf(__('Monthly Archives: %s', 'carrington-blueprint'), '<span>' . get_the_date(_x('F Y', 'monthly archives date format', 'carrington-blueprint')) . '</span>');
+		} elseif (is_year()) {
+			printf(__('Yearly Archives: %s', 'carrington-blueprint'), '<span>' . get_the_date(_x('Y', 'yearly archives date format', 'carrington-blueprint')) . '</span>');
+		} elseif (is_tag()) {
+			printf(__('Tag Archives: %s', 'carrington-blueprint'), '<span>' . single_tag_title('', false ) . '</span>');
+		} elseif (is_category()) {
+			printf(__('Category Archives: %s', 'carrington-blueprint'), '<span>' . single_cat_title('', false ) . '</span>');
+		} else {
+			_e('Blog Archives', 'carrington-blueprint');
+		}
+	?></h1>
 
+	<?php
+		// Show an optional category description
+		if (is_category) {
+			$category_description = category_description();
+			if ($category_description) {
+				echo '<div class="archive-description">' . $category_description . '</div>';
+			}
+		} elseif (is_tag()) {
+			$tag_description = tag_description();
+			if ($tag_description) {
+				echo '<div class="archive-description">' . $tag_description . '</div>';
+			}
+		}
+
+		// For the loop used, look in /loops
+		cfct_loop();
+
+		// Pagination
+		cfct_misc('nav-posts');
 	?>
+
 </div><!-- #primary -->
 
 <?php
