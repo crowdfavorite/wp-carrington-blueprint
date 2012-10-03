fc5fea832efa3feba6162c3f4024ad90a6600d97
Rona Kilmer <rona@crowdfavorite.com>
2012-10-03 11:37:28 -0600

    using time instead of date, adding edit post link, changing h1 to h2 in excerpt

--- excerpt/excerpt-default.php
+++ excerpt/excerpt-default.php
@@ -21,8 +21,8 @@ if (CFCT_DEBUG) { cfct_banner(__FILE__); }
 
 <article id="post-<?php the_ID() ?>" <?php post_class('excerpt clearfix') ?>>
 	<header class="entry-header">
-		<h1 class="entry-title"><a href="<?php the_permalink() ?>"  title="<?php printf( esc_attr__( 'Permalink to %s', 'carrington-blueprint' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title() ?></a></h1>
-		<time class="entry-date" datetime="<?php the_time('c'); ?>" pubdate><?php the_date(); ?></time>
+		<h2 class="entry-title"><a href="<?php the_permalink() ?>"  title="<?php printf( esc_attr__( 'Permalink to %s', 'carrington-blueprint' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title() ?></a></h2>
+		<time class="entry-date" datetime="<?php the_time('c'); ?>" pubdate><?php the_time(get_option('date_format')); ?></time>
 	</header>
 	<div class="entry-content">
 		<?php
@@ -34,5 +34,8 @@ if (CFCT_DEBUG) { cfct_banner(__FILE__); }
 			printf(__('<p><strong>Categories</strong> %s</p>', 'carrington-blueprint'), get_the_category_list(', '));
 			the_tags(__('<p><strong>Tags</strong> ', 'carrington-blueprint'), ', ', '</p>');
 		?>
+		<p><?php edit_post_link(); ?></p>
 	</div>
 </article><!-- .post -->
+
+
