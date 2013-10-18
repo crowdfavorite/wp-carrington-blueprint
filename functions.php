8fbd493e8fc6557df92bfe70727f191446856618
Rona Kilmer <Rona@crowdfavorite.com>
2013-10-18 10:14:16 -0600

    removed old iphone scaling fix, no longer something to worry about.

--- functions.php
+++ functions.php
@@ -136,7 +136,6 @@ function cfct_load_assets() {
 	wp_enqueue_script('modernizr', $cfct_assets_url . 'js/modernizr-2.6.2.min.js', array(), CFCT_URL_VERSION);
 	wp_enqueue_script('placeholder', $cfct_assets_url . 'js/jquery.placeholder.min.js', array('jquery'), CFCT_URL_VERSION);
 	wp_enqueue_script('script', $cfct_assets_url . 'js/script.js', array('jquery', 'placeholder'), CFCT_URL_VERSION);
-	wp_enqueue_script('iphone-scaling-fix', $cfct_assets_url . 'js/ios-orientationchange-fix.js', array(), CFCT_URL_VERSION, true);
 }
 add_action('wp_enqueue_scripts', 'cfct_load_assets');
 
