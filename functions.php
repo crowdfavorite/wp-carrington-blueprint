8733159139570990efa58869f089390cbf5bcbe2
Craig Freeman <craig@crowdfavorite.com>
2014-02-03 17:03:59 -0700

    Updated to Modernizr 2.7.1

--- functions.php
+++ functions.php
@@ -133,7 +133,7 @@ function cfct_load_assets() {
 	}
 
 	// Scripts
-	wp_enqueue_script('modernizr', $cfct_assets_url . 'js/modernizr-2.6.2.min.js', array(), CFCT_URL_VERSION);
+	wp_enqueue_script('modernizr', $cfct_assets_url . 'js/modernizr-2.7.1.min.js', array(), CFCT_URL_VERSION);
 	wp_enqueue_script('placeholder', $cfct_assets_url . 'js/jquery.placeholder.min.js', array('jquery'), CFCT_URL_VERSION);
 	wp_enqueue_script('script', $cfct_assets_url . 'js/script.js', array('jquery', 'placeholder'), CFCT_URL_VERSION);
 }
