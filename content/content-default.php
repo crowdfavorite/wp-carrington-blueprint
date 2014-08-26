3df520d7f9c0aa733dd1749c7574e9320551b9d1
Rona Kilmer <Rona@crowdfavorite.com>
2014-08-26 11:13:18 -0600

    rw

--- content/content-default.php
+++ content/content-default.php
@@ -35,8 +35,8 @@ if (CFCT_DEBUG) { cfct_banner(__FILE__); }
 	</div>
 	<div class="entry-footer entry-meta">
 		<?php
-			printf( __( '<p><strong>Categories:</strong> %s</p>', 'carrington-blueprint'), get_the_category_list( ', ' ) );
-			the_tags( __( '<p><strong>Tags:</strong> ', 'carrington-blueprint'), ', ', '</p>' );
+			printf( __( '<p><strong>Categories:</strong> %s</p>', 'carrington-blueprint' ), get_the_category_list( ', ' ) );
+			the_tags( __( '<p><strong>Tags:</strong> ', 'carrington-blueprint' ), ', ', '</p>' );
 		?>
 	</div>
 </article><!-- .post -->
